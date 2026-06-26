0eee16a55bb1f3cf941666c321ac696e641d21c6
Rahul Shaik <r161731@rguktrkv.ac.in>
2026-06-26 13:26:35 +0530

    Set-up server

---
+++ src/server.js
@@ -0,0 +1,19 @@
+require("dotenv").config(); //configing the ENV
+const express = require("express");
+const { SERVER_PORT } = require("./config/env");
+const app = express();
+const PORT = SERVER_PORT;
+
+app.use(express.json()); //middleware parsing the request JSONbody
+app.use(express.urlencoded({ extended: true })); //pasting the body(data) for FORM_DATA
+
+//Middleware to get the REQUESTS
+app.use((req, res, next) => {
+  console.log(`Request URL : ${req.url} with Request METHOD : ${req.method}`);
+  console.log(`Request BODY : ${JSON.stringify(req.body)}`);
+  next();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening at ${PORT}`);
+});
